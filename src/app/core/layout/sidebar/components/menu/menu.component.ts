172cf867eac3bee1a67dc6d55398bcf0379d899d
JavierSR99 <javiolopepicanela@gmail.com>
2023-10-19 10:30:58 +0200

    sidebar menu router navigation

--- src/app/core/layout/sidebar/components/menu/menu.component.ts
+++ src/app/core/layout/sidebar/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuIcon, MenuItemType } from 'src/app/core/models/enum/menu-item.enum';
 import { IMenuItem } from 'src/app/core/models/interfaces/menu-item.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'jav-menu',
@@ -11,16 +12,24 @@ export class MenuComponent implements OnInit {
   // #region VARIABLES
   public menuItems: IMenuItem[] = [
     { label: 'Pages', icon: MenuIcon.folder, type: MenuItemType.parent, parentOpen: true },
-    { label: 'HOME', icon: MenuIcon.html, type: MenuItemType.child, link: '/home' },
+    { label: 'HOME', icon: MenuIcon.html, type: MenuItemType.child, link: '/' },
     { label: 'CONTACT', icon: MenuIcon.scss, type: MenuItemType.child, link: '/contact' },
     { label: 'Core', icon: MenuIcon.foldercheck, type: MenuItemType.parent, parentOpen: false },
     { label: 'Shared', icon: MenuIcon.foldercheck, type: MenuItemType.parent, parentOpen: false }
   ];
   // #endregion
 
-  constructor() { }
+  constructor(
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public goToRoute(item: IMenuItem): void {
+    if (item.link) {
+      this._router.navigate([item.link]);
+    }
   }
 
 }
