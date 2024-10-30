158d73c4bfd70ee5a737cec12869ba6f91d31eb1
JavierSR99 <javiolopepicanela@gmail.com>
2024-10-30 12:28:25 +0100

    home-page typing animation v1.0

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,6 +8,8 @@ import TypeIt from "typeit";
 })
 export class HomeComponent implements OnInit {
 
+  public textComplete: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -32,14 +34,21 @@ export class HomeComponent implements OnInit {
       loop: false,
       speed: 75,
       waitUntilVisible: true,
+      afterComplete: () => {
+        this.textComplete = true;
+      }
     }).type("soy", { delay: 200 })
     .pause(200)
+    .type("<span class='u-color-comp2'> Javier Sanz </span>")
+    .pause(1000)
+    .delete(12)
     .type("<span class='u-color-comp1'> desarrollador web</span>")
     .pause(1000)
     .delete(18)
     .type("<span class='u-color-comp1'> arquitecto BEMIT </span>")
     .pause(1000)
     .delete(17)
+    .type("<span class='u-color-comp2'> Javier Sanz </span>")
     .go();
   }
 }
