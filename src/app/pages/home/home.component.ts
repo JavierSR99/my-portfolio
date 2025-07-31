eb75b6b5fe30e458a7da3c69d485c93847a33e4e
JavierSR99 <javiolopepicanela@gmail.com>
2025-07-31 13:13:00 +0200

    add layout changes to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,35 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LanguageService } from 'src/app/core/services/language.service';
 import TypeIt from "typeit";
+import { ITypeitText } from './interfaces/typeit-text.interface';
 
 @Component({
   selector: 'jav-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  //#region READONLY VARIABLES
+  private readonly typeitSpanish: ITypeitText = {
+    hi: "Hola,",
+    iam: "soy",
+    name: "Javier Sanz",
+    job1: "desarrollador web",
+    job2: "arquitecto BEMIT"
+  };
+
+  private readonly typeitEnglish: ITypeitText = {
+    hi: "Hello,",
+    iam: "I am",
+    name: "Javier Sanz",
+    job1: "web developer",
+    job2: "BEMIT architect"
+  };
+  //#endregion
+
+  //#region VARIABLES
+  private activeLang: "es" | "en" | "" = "";
   public textComplete: boolean = false;
 
-  constructor() { }
+  private listObservers$: Array<Subscription> = [];
+  //#endregion
+
+  //#region CONSTRUCTOR & LIFECYCLE HOOKS
+  constructor(private _ls: LanguageService) { }
 
   ngOnInit(): void {
-    this.generateFirstString();
+    const observableLang$ = this.getLang();
+    
+    this.activeLang == "es" ? this.generateFirstString(this.typeitSpanish) : this.generateFirstString(this.typeitEnglish);
+
   }
 
-  private generateFirstString(): void {
+  ngOnDestroy() {
+    this.listObservers$.forEach(u => u.unsubscribe());
+  }
+  //#endregion
+
+  //#region PRIVATE METHODS
+  private getLang(): Subscription {
+    return this._ls.activeLanguage$.subscribe((lang: string) => {
+      if (this.activeLang == "") { this.activeLang = lang as "es" | "en"; }
+      else if (this.activeLang !== lang) {
+
+      }
+
+
+    });
+  }
+
+  private generateFirstString(lang: ITypeitText): void {
 
     new TypeIt("#hi-message", {
       cursor: false,
       speed: 100,
       waitUntilVisible: true,
       afterComplete: async () => {
-        this.generateSecondString();
+        this.generateSecondString(lang);
       }
-    }).type("Hola,", { delay: 200 })
+    }).type(lang.hi, { delay: 200 })
     .go();
   }
 
-  private generateSecondString(): void {
+  private generateSecondString(lang: ITypeitText): void {
     new TypeIt("#iam-message", {
       loop: false,
       speed: 75,
@@ -37,18 +84,19 @@ export class HomeComponent implements OnInit {
       afterComplete: () => {
         this.textComplete = true;
       }
-    }).type("soy", { delay: 200 })
+    }).type(lang.iam, { delay: 200 })
     .pause(200)
-    .type("<span class='u-color-comp2'> Javier Sanz </span>")
+    .type(`<span class='u-color-comp2'> ${lang.name} </span>`)
     .pause(1000)
     .delete(12)
-    .type("<span class='u-color-comp1'> desarrollador web</span>")
+    .type(`<span class='u-color-comp1'> ${lang.job1} </span>`)
     .pause(1000)
     .delete(18)
-    .type("<span class='u-color-comp1'> arquitecto BEMIT </span>")
+    .type(`<span class='u-color-comp1'> ${lang.job2} </span>`)
     .pause(1000)
     .delete(17)
-    .type("<span class='u-color-comp2'> Javier Sanz </span>")
+    .type(`<span class='u-color-comp2'> ${lang.name} </span>`)
     .go();
   }
+  //#endregion
 }
---
+++ src/app/pages/home/interfaces/typeit-text.interface.ts
@@ -0,0 +1,7 @@
+export interface ITypeitText {
+    hi: string;
+    iam: string;
+    name: string;
+    job1: string;
+    job2: string;
+}
